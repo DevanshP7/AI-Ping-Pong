3f082afe7cdec1c759ca0f81898cc5277454e5b1
DevanshP7 <79050720+DevanshP7@users.noreply.github.com>
2022-06-30 18:58:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -24,12 +24,22 @@ var ball = {
 function setup(){
   var canvas =  createCanvas(700,600);
   canvas.parent('canvas');
+
+  video = createCapture(VIDEO);
+  video.size(700,600);
+  video.hide();
+
+  posenet = ml5.poseNet(video, modelloaded);
+}
+
+function modelloaded(){
+  console.log('Model Loaded!');
 }
 
 
 function draw(){
+ image(video,0,0,700,600);
 
- background(0); 
 
  fill("black");
  stroke("black");
